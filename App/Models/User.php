8084b7d236dae341e81688a68d79aeaf66e82a57
kontseus <konceus1@gmail.com>
2022-06-02 16:21:02 +0300

    [hw-5] Exceptions

--- App/Models/User.php
+++ App/Models/User.php
@@ -2,10 +2,50 @@
 
 namespace App\Models;
 
+
+use Exception;
+
 class User
 {
-    public function __construct()
+
+    private int $id;
+    private string $password;
+
+    public function __construct($id, $password)
     {
-        echo get_class() . "\n";
+
+        if (!is_numeric($id)) {
+            throw new Exception("\n ID should be integer");
+        }
+        if (strlen($password) > 8) {
+            throw new Exception("\n Password should be not longer than 8 symbols");
+        }
+        $this->id = $id;
+        $this->password = $password;
     }
+
+    public function getUserData()
+    {
+        return 'ID is ' . $this->getId() . ' and password is ' .  $this->getPassword() . '.';
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
 }
+
+/*
+    Создайте класс User с полями id (должно содержать только число) и password (длина поля должна быть
+
+    не более 8 символов) и методом getUserData, который возвращает id и email. Обработайте исключения,
+
+    когда id содержит не число и password содержит более 8 символов, при этом выведите сообщение
+
+    исключения, файл в котором данное исключение возникло и номер строки.
+*/
--- index.php
+++ index.php
@@ -1,26 +1,12 @@
 <?php
 
-require_once  __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\OrdersController;
-use App\Http\Controllers\MainController;
-use App\Http\Helpers\ImageHelper;
-use App\Models\Order;
 use App\Models\User;
-use App\Models\Product;
 
-
-echo '<pre>';
-new DashboardController();
-new OrdersController();
-new MainController();
-
-new ImageHelper();
-
-new Order();
-new Product();
-new User();
-
-new Test();
-echo '</pre>';
+try {
+    $user = new User(15, 'pas123');
+    echo $user->getUserData();
+} catch (Exception $e) {
+    die($e->getMessage() . ' on line ' . $e->getLine() . ' in file ' . $e->getFile());
+}
