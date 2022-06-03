6d468024d1b5df7c70828035518c0015073391e9
kontseus <konceus1@gmail.com>
2022-06-03 21:31:21 +0300

    initial master branch

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once  __DIR__ . '/vendor/autoload.php';
-
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\OrdersController;
-use App\Http\Controllers\MainController;
-use App\Http\Helpers\ImageHelper;
-use App\Models\Order;
-use App\Models\User;
-use App\Models\Product;
-
-
-echo '<pre>';
-new DashboardController();
-new OrdersController();
-new MainController();
-
-new ImageHelper();
-
-new Order();
-new Product();
-new User();
-
-new Test();
-echo '</pre>';
